121be4999e35b14580359ed5f8ae9e4a95600f48
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-08-13 18:30:19 +0530

    sinlge class, multi class, Anomous

---
+++ OOPs_class02.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class cybrom
+{
+    int a =90;
+    //cout<<a;
+    public: void show()
+    {
+        cout<<a<<endl;
+    }
+}job;
+class j2
+{
+    public:void display()
+    {
+        cout<< "Display function";
+    };
+};
+int main()
+{
+    cybrom obj;
+    job.show();
+    j2 ob;
+    ob.display();
+
+}               //(factroial, power, table)
+
+//Types of classes - (i) Single class - if a program having only one class is called a single class.
+//                 (ii) Anomymous Classs - if a program having a class which doesent have its own name is call Anomymous.
+//                  (iii) Multi Class - If a class haveing a more then one classes is know as multi-class and we have to create an object of each class to call a member function.
+//                  (iv)
+
+
